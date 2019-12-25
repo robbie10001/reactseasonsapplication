906e58fead27aab71716159f4959c65ab45ba7c2
robbie10001/rn <robbie_c1000@hotmail.com>
2019-12-26 10:57:55 +1100

    icons not renderinggit commit -m

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -11,10 +11,15 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
-    const text = season === "winter" ? "Burr, it's chilly!" : "Let's hit the beach!"
+    const text = 
+        season === "winter" ? "Burr, it's chilly!" : "Let's hit the beach!";
+    const icon = season === "winter" ? "snowflake" : "sun"; 
+    
     return (
     <div>
+        <i className={`${icon} icon`} />
         <h1>{text}</h1>
+        <i className={`${icon} icon`} />
     </div>
     );
 };
